86695c1b678472946d87d891fbe189bce0b516e3
issu478 <issumr860@gmail.com>
2025-11-12 13:50:50 +0530

    Evill 〽️ D
    Updated SESSION_ID to a default value for testing.

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Paste Your seeson id here",
+SESSION_ID: process.env.SESSION_ID || "1AASWDpZ#8GwFZuT-Bho9repYqI67naqQWLVQvo_a-wKKD_o9mYk",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
