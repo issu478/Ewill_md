54ef61c1e4c218548637949d6a4cc2b75519ef11
issu478 <issumr860@gmail.com>
2025-11-16 18:32:54 +0530

    Evill 〽️ D

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "7pAVDRza#vexN90Mz0pprsp-ePfVwjYoQ0QBux_NUVoRmKbjRCC8",
+SESSION_ID: process.env.SESSION_ID || "joplia6J#Fwmlm9ocD2SaH0yzcRtVgUrx3ut9HZ_LoQLC2IcKgwM",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
